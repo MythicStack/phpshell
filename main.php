51a85d62e2534ee35c0923b5be67f60e19e8838a
John <110578058+MythicStack@users.noreply.github.com>
2024-08-11 23:02:02 -0800

    Create main.php
    File for WP Plugin creation.

---
+++ main.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: MythicStack - PL
+Plugin URI: https://github.com/MythicStack
+Description: Base WP Shell Plugin - Malicious
+Version: 1.0.0
+Author: MythicStack
+Author URI: https://github.com/MythicStack
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+?>
